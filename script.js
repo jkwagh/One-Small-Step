8f00ca289bc789f3f619e6dd0f620ba719e52db8
Jeremiah <kwagh.jeremiah@gmail.com>
2023-09-27 12:52:36 -0400

    adding links and updating text

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+h2.textContent = "Jeremy Kwagh is a former accountant pursuing a career as a software engineer.";
 document.querySelector("body").appendChild(h2);
